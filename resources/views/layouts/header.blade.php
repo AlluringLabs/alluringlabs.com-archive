ae975a49576ff2ca2f0b7438282f449e265bb223
Joshua Kidd <kidd.josh.343@gmail.com>
2016-07-19 15:39:20 -0600

    Added services pages

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,28 @@
-<div class="top-bar"></div>
+<div class="top-bar">
+  <div class="container">
+    <div class="contact pull-xs-right">
+      <div class="item">
+        <i class="fa fa-phone"></i>
+        <a href="tel:7576553387">757.655.3387</a>
+      </div>
+      <div class="item">
+        <i class="fa fa-envelope"></i>
+        <a href="mailto:hello@alluringlabs.com">hello@alluringlabs.com</a>
+      </div>
+      <div class="item">
+        <i class="fa fa-twitter"></i>
+        <a href="https://twitter.com/alluring_labs" target="_blank"> @alluring_labs</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 <header class="main-header" id="header">
   <div class="container">
-    {{-- <div class="row">
-      <div class="col-md-6 col-"></div>
-    </div> --}}
-
-
     <div class="pull-md-left">
       <div class="site-name">
         <span>Alluring</span>
-        {{-- @include('partials.logo-svg') --}}
+        @include('partials.logo-svg')
         <span>Labs</span>
       </div>
     </div>
--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -8,8 +8,11 @@
             <img src="http://placehold.it/350x200" alt="">
             <div class="info">
               <div class="title">Development</div>
-              <div class="description"></div>
-              <div class="more"><a href="#">Learn More</a></div>
+              <div class="description">
+                <p>Using the latest and greatest technologies, we can help you improve your existing web applications or make your new idea(s) come to full fruition.</p>
+                <p>We also enjoy our sanity, and making the end user happy, so the code we write is concise, clean and easily debuggable.</p>
+              </div>
+              <div class="more"><a href="/services/development">Learn More</a></div>
             </div>
           </div>
         </div>
@@ -18,8 +21,11 @@
             <img src="http://placehold.it/350x200" alt="">
             <div class="info">
               <div class="title">UI &amp; UX Design</div>
-              <div class="description"></div>
-              <div class="more"><a href="#">Learn More</a></div>
+              <div class="description">
+                <p>All great systems need to be accompanied by an alluring interface and a near perfect user experience.</p>
+                <p>We can bring your idea(s) to life in a visually appealing way; don't make your users deal with a poor experience!</p>
+              </div>
+              <div class="more"><a href="/services/design">Learn More</a></div>
             </div>
           </div>
         </div>
@@ -28,8 +34,11 @@
             <img src="http://placehold.it/350x200" alt="">
             <div class="info">
               <div class="title">Consulting</div>
-              <div class="description"></div>
-              <div class="more"><a href="#">Learn More</a></div>
+              <div class="description">
+                <p>Don't know where to start when building your new system? Perhaps you're stuck in the middle of a project, not knowing where to go?</p>
+                <p>Everyone needs a bit of guidance here and there; let our years of experiences guide the path to breathing new life into your old applications, or help pave the way for new ones!</p>
+              </div>
+              <div class="more"><a href="/services/consulting">Learn More</a></div>
             </div>
           </div>
         </div>
